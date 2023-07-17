90991d3fff899556d43a7e224e0c07367aa63a91
AbduHosein <ahosein57@gmail.com>
2023-07-17 18:09:16 -0400

    Add responsive background

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import data from '../data.json';
 import React from 'react';
 import instaicon from "../public/images/logo-instagram.svg";
+import img from "../public/mountain.jpg"
 import ReactDOM from 'react-dom';
 import { SocialIcon } from 'react-social-icons';
 
@@ -10,32 +11,43 @@ import { SocialIcon } from 'react-social-icons';
 export default function Home() {
   return (
     <div className='flex items-center flex-col mx-auto
-     w-full justify-center mt-4 px-8'>
-      <ShareCard className="flex-row">
-        
-      </ShareCard>
-      <Image
-        className='rounded-full'
-        alt={data.name}
-        src={data.avatar}
-        width={95}
-        height={95}>
-      </Image>
-      <h1 className='font-bold mt-4 text-2xl'>@mainehousemusic</h1>
-      <h1 className='font-normal mt-1 mb-7 text-lg'>Diego Smith - DJ based in Portland, Maine</h1>
-      {data.links.map((link) => (
-        <LinkCard key={link.href} {...link} />)
-      )}
-      <a href="https://instagram.com/mainehousemusic/"><Image
-          priority
-          src={instaicon}
-          height={80}
-          width={50}
-          alt='Follow on Instagram'        
-        />
-         </a>
-    </div>
-
+      w-full justify-center mt-4 px-8'>
+         <div style={{
+        zIndex: -1,
+        position: "fixed",
+        width: "100vw",
+        height: "160vh"
+        }}>
+          <Image 
+            src={img}
+            alt="Mountains with snow"
+            layout="fill"
+            objectFit='cover'
+          />
+        </div>
+        <ShareCard className="flex-row">
+        </ShareCard>
+        <Image
+          className='rounded-full'
+          alt={data.name}
+          src={data.avatar}
+          width={95}
+          height={95}>
+        </Image>
+        <h1 className='font-bold mt-4 text-2xl'>@mainehousemusic</h1>
+        <h1 className='font-semibold mt-1 mb-7 text-lg'>Diego Smith - DJ based in Portland, Maine</h1>
+        {data.links.map((link) => (
+          <LinkCard key={link.href} {...link} />)
+        )}
+        <a href="https://instagram.com/mainehousemusic/"><Image
+            priority
+            src={instaicon}
+            height={80}
+            width={50}
+            alt='Follow on Instagram'        
+          />
+          </a>
+      </div>
   )
 }
 
